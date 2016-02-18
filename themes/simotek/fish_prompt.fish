fc15ef479f1a0f61f9f979fdda2c4d04bf254bfb
Simon Lees <simon@simotek.net>
2016-02-18 21:35:04 +1030

    Use new color palette for now

--- themes/simotek/fish_prompt.fish
+++ themes/simotek/fish_prompt.fish
@@ -2,14 +2,14 @@
 function _simotek_theme_user --description 'Print user name'
     switch $USER
         case root
-            printf '%s%s%s' (set_color red) $USER (set_color normal)
+            printf '%s%s%s' (set_color (red)) $USER (set_color normal)
         case '*'
-            printf '%s%s%s' (set_color 5fff5f) $USER (set_color normal)
+            printf '%s%s%s' (set_color (green)) $USER (set_color normal)
     end
 end
 
 function _simotek_theme_hostname --description 'Print the hostname'
-    printf '%s%s%s' (set_color 515fff) (hostname|cut -d . -f 1) (set_color normal)
+    printf '%s%s%s' (set_color (blue)) (hostname|cut -d . -f 1) (set_color normal)
 end
 
 function _simotek_theme_cwd --description 'Print current working directory'
@@ -17,16 +17,16 @@ function _simotek_theme_cwd --description 'Print current working directory'
 end
 
 function fish_prompt --description 'Write out the prompt'
-   
-    set -l ___symbol_color af0087   
- 
+
+    set -l ___symbol_color (red)
+
     # print the prompt
     set -l ___at (printf '%s@%s' (set_color $___symbol_color) (set_color normal))
-    set -l ___arrow (printf '%s@%s' (set_color 870087) (set_color normal))
+    set -l ___arrow (printf '%s@%s' (set_color $___symbol_color) (set_color normal))
     set -l ___host (printf '%s%s%s' (_simotek_theme_user) $___at (_simotek_theme_hostname))
     set -l ___cwd (_simotek_theme_cwd)
     #set -l ___vcs (printf '%s%s' (_simotek_theme_vcs))
-    set -l ___prompt_char (printf '%s➤ %s' (set_color $___symbol_color) (set_color normal))  
+    set -l ___prompt_char (printf '%s➤ %s' (set_color $___symbol_color) (set_color normal))
 
     printf '%s %s%s %s' $___host $___cwd $___vcs $___prompt_char
 end
