3fca7eb36d31ea4dd3d9fc8697cc60642a2a5a30
Simon Lees <simon@simotek.net>
2016-02-18 21:31:47 +1030

    Add color module for palette

---
+++ plugins/colors/colors.fish
@@ -0,0 +1,48 @@
+function colors -d "This function does nothing other then load all the other color functions (red, blue, green, yellow etc)"
+
+end
+
+
+function blue -d "The color blue"
+    echo "#3498D8"
+end
+
+function green -d "The color green"
+    echo "#2ECC71"
+end
+
+function yellow -d "The color yellow"
+    echo "#F1C40F"
+end
+
+function red -d "The color red"
+    echo "#E74C3C"
+end
+
+function purple -d "The color purple"
+    echo "#9B59B6"
+end
+
+function orange -d "The color orange"
+    echo "#E67E22"
+end
+
+function cyan -d "The color cyan"
+    echo "#1ABC9C"
+end
+
+function dark -d "The color dark"
+    echo "#34495E"
+end
+
+function white -d "The color white"
+    echo "#ECF0F1"
+end
+
+function offwhite -d "The color offwhite"
+    echo "#95A5A6"
+end
+
+function offwhite_alt -d "The color offwhite"
+    echo "#7F8C8D"
+end
