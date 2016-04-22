1bddea94ba1da9d2dceb6026874ebaa918be9553
Simon Lees <sflees@suse.de>
2016-04-22 15:33:50 +0930

    Fix greeting colors

--- themes/simotek/fish_greeting.fish
+++ themes/simotek/fish_greeting.fish
@@ -5,9 +5,9 @@ function fish_greeting --description 'Print the shell greeting'
     set -l c_n '\x1b[92m'
     set -l c_w '\x1b[93m'
 
-    set -l location (printf "\x1b[92mWelcome to \x1b[93m%s" (hostname))
-    set -l system (printf "\x1b[92mRunning \x1b[93m%s\x1b[92m on \x1b[93m%s" (uname -mrs) (tty | sed -e 's/.*tty\(.*\)/\1/'))
-    set -l datetime (printf "\x1b[92mIt is \x1b[93m%s\x1b[92m (%s) on \x1b[93m%s" (date +%T) (date +%Z) (date +%F))
+    set -l location (printf "\x1b[92mWelcome to \x1b[95m%s" (hostname))
+    set -l system (printf "\x1b[92mRunning \x1b[95m%s\x1b[92m on \x1b[95m%s" (uname -mrs) (tty | sed -e 's/.*tty\(.*\)/\1/'))
+    set -l datetime (printf "\x1b[92mIt is \x1b[95m%s\x1b[92m (%s) on \x1b[95m%s" (date +%T) (date +%Z) (date +%F))
 
     printf "\n  %s\n  %s\n  %s\n\n" $location $system $datetime
     
