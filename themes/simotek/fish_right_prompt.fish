de1db9727c3f5427dd39aa4f9d8be3f909f6e7a8
Simon Lees <sflees@suse.de>
2016-04-27 10:09:12 +0930

    Add quilt support to the right prompt

--- themes/simotek/fish_right_prompt.fish
+++ themes/simotek/fish_right_prompt.fish
@@ -50,31 +50,31 @@ function fish_right_prompt --description 'Write out the right-hand prompt'
 
     if git rev-parse --is-inside-work-tree > /dev/null ^ /dev/null
 
-  # These are blank for the right hand prompt
-  set PROMPT_START ""
-	set PROMPT_END ""
+        # These are blank for the right hand prompt
+        set PROMPT_START ""
+    	set PROMPT_END ""
 
-	set -e __CURRENT_GIT_STATUS
-	set gitstatus "$__GIT_PROMPT_DIR/gitstatus.py"
+    	set -e __CURRENT_GIT_STATUS
+    	set gitstatus "$__GIT_PROMPT_DIR/gitstatus.py"
 
-	set _GIT_STATUS (python $gitstatus)
-	set __CURRENT_GIT_STATUS $_GIT_STATUS
+    	set _GIT_STATUS (python $gitstatus)
+    	set __CURRENT_GIT_STATUS $_GIT_STATUS
 
-	set __CURRENT_GIT_STATUS_PARAM_COUNT (count $__CURRENT_GIT_STATUS)
+    	set __CURRENT_GIT_STATUS_PARAM_COUNT (count $__CURRENT_GIT_STATUS)
 
-	if not test "0" -eq $__CURRENT_GIT_STATUS_PARAM_COUNT
-	    set GIT_BRANCH $__CURRENT_GIT_STATUS[1]
-	    set GIT_REMOTE "$__CURRENT_GIT_STATUS[2]"
-	    if contains "." "$GIT_REMOTE"
-		set -e GIT_REMOTE
-	    end
-	    set GIT_STAGED $__CURRENT_GIT_STATUS[3]
-	    set GIT_CONFLICTS $__CURRENT_GIT_STATUS[4]
-	    set GIT_CHANGED $__CURRENT_GIT_STATUS[5]
-	    set GIT_UNTRACKED $__CURRENT_GIT_STATUS[6]
-	    set GIT_STASHED $__CURRENT_GIT_STATUS[7]
-	    set GIT_CLEAN $__CURRENT_GIT_STATUS[8]
-	end
+    	if not test "0" -eq $__CURRENT_GIT_STATUS_PARAM_COUNT
+    	    set GIT_BRANCH $__CURRENT_GIT_STATUS[1]
+    	    set GIT_REMOTE "$__CURRENT_GIT_STATUS[2]"
+    	    if contains "." "$GIT_REMOTE"
+    		set -e GIT_REMOTE
+    	    end
+    	    set GIT_STAGED $__CURRENT_GIT_STATUS[3]
+    	    set GIT_CONFLICTS $__CURRENT_GIT_STATUS[4]
+    	    set GIT_CHANGED $__CURRENT_GIT_STATUS[5]
+    	    set GIT_UNTRACKED $__CURRENT_GIT_STATUS[6]
+    	    set GIT_STASHED $__CURRENT_GIT_STATUS[7]
+    	    set GIT_CLEAN $__CURRENT_GIT_STATUS[8]
+    	end
 
         set STATUS " $GIT_PROMPT_PREFIX$GIT_PROMPT_BRANCH$GIT_BRANCH$WHITE"
 
@@ -111,6 +111,21 @@ function fish_right_prompt --description 'Write out the right-hand prompt'
         set STATUS "$STATUS$GIT_PROMPT_SUFFIX"
 
         echo -e "$PROMPT_START$STATUS$PROMPT_END"
+    else if test -r "$PWD/series" -a -d "$PWD/.pc/"
+        # Get current patch name 
+        if test -r "$PWD/.pc/applied-patches"
+            set -l __tmp (tail -1 .pc/applied-patches)
+            set __quilt_current_patch_name (echo -e "\x1b[92m$__tmp")
+            set __quilt_current_patch_number (cat .pc/applied-patches | wc -l)
+        else
+            set __quilt_current_patch_name (echo -e "\x1b[33mNo patches applied")
+            set __quilt_current_patch_number "0"
+        end
+        
+        set -l __file_count (cat series | wc -l)
+        # There is 4 lines of comments at the start of the file
+        set -l __patch_count (echo "$__file_count-4" | bc)
+        printf '\x1b[40m\x1b[91m[%s\x1b[91m|\x1b[34m%s\x1b[33m/\x1b[92m%s\x1b[91m]%s' $__quilt_current_patch_name $__quilt_current_patch_number $__patch_count (set_color normal)
     else
         printf ' \x1b[40m\x1b[91m[%s%s\x1b[91m|%s%s\x1b[91m]%s' (set_color (offwhite_alt)) (date) (set_color 777) $_display_status (set_color normal)
     end
