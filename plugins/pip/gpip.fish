cbd6460ca0fb27ce38be3acdce21e969baf95642
Justin Mayer <entroP@gmail.com>
2018-08-18 12:43:40 +0200

    Disable require_virtualenv in gpip.fish for Pip 10+
    Also disables Pip version check; constantly seeing new version
    warnings gets annoying fast if you prefer to upgrade Pip infrequently.

--- plugins/pip/gpip.fish
+++ plugins/pip/gpip.fish
@@ -1,3 +1,3 @@
-function gpip -d "Manage globally-installed Python packages"
-    env PIP_REQUIRE_VIRTUALENV="" pip $argv
+function gpip --wraps pip -d "Manage globally-installed Python packages"
+    env PIP_REQUIRE_VIRTUALENV="0" pip $argv --disable-pip-version-check
 end
